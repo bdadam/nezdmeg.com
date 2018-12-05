558cd5af67d1936766d3947a4126540bed97eaf1
bdadam <me@bdadam.com>
2018-12-05 22:46:23 +0100

    Setting title attribute on video embed iframe

--- components/v-ideo.vue
+++ components/v-ideo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="video-player">
-        <iframe :src="src" class="video-player__iframe"></iframe>
+        <iframe :src="src" title="Beágyazott video"></iframe>
     </div>
 </template>
 <script>
@@ -13,22 +13,22 @@ export default {
     },
 };
 </script>
-<style>
+<style lang="scss" scopoed>
 .video-player {
-    /* display: block; */
     position: relative;
     height: 0;
     padding-top: 56.25%;
     background-color: #000;
-}
-.video-player__iframe {
-    display: block;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+
+    iframe {
+        display: block;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
 }
 </style>
