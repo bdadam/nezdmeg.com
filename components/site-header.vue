309c125be9b20747a08bd9724b778f62981c6081
bdadam <me@bdadam.com>
2018-12-05 16:30:07 +0100

    Cleaned up site-header styles

--- components/site-header.vue
+++ components/site-header.vue
@@ -7,23 +7,11 @@
         </div>
     </div>
 </template>
-<style>
+<style lang="scss">
 .site-header {
-    padding: 20px 0;
-    /* text-align: center; */
+    padding: 12px 0 10px;
     border-bottom: 1px solid #cdcdcd;
-    margin-bottom: 20px;
+    background-color: #fff;
 }
 
-/* .site-header__container {
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 0 20px;
-}
-
-@media (min-width: 1240px) {
-    .site-header__container {
-        padding: 0;
-    }
-} */
 </style>
