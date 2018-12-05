0ba504d231b07eab2a14c978c64f0f65f481262f
bdadam <me@bdadam.com>
2018-12-05 16:39:27 +0100

    Fixed site-header styles

--- components/site-header.vue
+++ components/site-header.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="site-header">
         <div class="page-container">
-            <nuxt-link to="/">
+            <nuxt-link to="/" class="brand-link">
                 <img src="~/assets/logo.png">
             </nuxt-link>
         </div>
     </div>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
 .site-header {
     padding: 12px 0 10px;
     border-bottom: 1px solid #cdcdcd;
     background-color: #fff;
 }
 
+.brand-link {
+    padding: 0 20px;
+}
 </style>
