0e829fb9ce9ddd72c06c728bf9669a21c79b4c0a
bdadam <me@bdadam.com>
2018-12-05 16:16:19 +0100

    Using Open Sans

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,10 @@ module.exports = {
     head: {
         meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
         link: [
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,600&amp;subset=latin-ext',
+            },
             // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ],
     },
